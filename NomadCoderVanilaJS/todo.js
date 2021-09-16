6e559b6fa1cbbe76cdf557a12d51fb9e72c149a4
Ahram-Kim <ahram0223@gmail.com>
2021-09-16 22:48:45 +0900

    #3-6 Making a To Do List part Two

--- NomadCoderVanilaJS/todo.js
+++ NomadCoderVanilaJS/todo.js
@@ -4,16 +4,32 @@ const toDoList = document.querySelector(".js-toDoList");
 
 const TODOS_LS = 'toDos';
 
+const toDos = [];
+
+function saveToDos() {
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
 function paintToDo(text) {
     // console.log(text);
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
-    delBtn.innerHTML = "X";
     const span = document.createElement("span");
+    const newId = toDos.length + 1;
+
+    delBtn.innerHTML = "X";
     span.innerText = text;
     li.appendChild(span);
     li.appendChild(delBtn);
+    li.id = newId;
     toDoList.appendChild(li);
+
+    const toDoObj = {
+        text: text,
+        id: newId
+    };
+    toDos.push(toDoObj);
+    saveToDos();
 }
 
 function handleSubmit(event) {
@@ -24,10 +40,15 @@ function handleSubmit(event) {
 }
 
 function loadToDos() {
-    const toDos = localStorage.getItem(TODOS_LS);
+    const loadedToDos = localStorage.getItem(TODOS_LS);
 
-    if(toDos !== null) {
-
+    if(loadedToDos !== null) {
+        // console.log(loadedToDos);
+        const parsedToDos = JSON.parse(loadedToDos);
+        // console.log(parsedToDos);
+        parsedToDos.forEach(function(toDo) {
+            paintToDo(toDo.text);
+        });
     }
 }
 
